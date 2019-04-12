9c02b686c5e5dc1f9f1ec043ddd7c5b04f6a5114
Callum Cooper <cr3ativ3d3v3lop3r@gmail.com>
2019-04-12 15:48:45 +0100

    Basic setup

---
+++ include/FileReader.h
@@ -0,0 +1,19 @@
+#ifndef FILE_READER_H_
+#define FILE_READER_H_
+
+#include <iostream>
+
+using namespace std;
+
+class FileReader
+{
+    public:
+        FileReader();
+        ~FileReader();
+
+        void sayHello();
+
+    private:
+};
+
+#endif
---
+++ src/FileReader.cpp
@@ -0,0 +1,14 @@
+#include "../include/FileReader.h"
+
+FileReader::FileReader()
+{
+}
+
+FileReader::~FileReader()
+{
+}
+
+void FileReader::sayHello()
+{
+    cout << "Hello World!" << endl;
+}
